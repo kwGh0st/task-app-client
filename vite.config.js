0aaa0f24884e96497513e4252f96e9e4bb15f725
Karol Programuje <karol341654@gmail.com>
2024-02-26 20:18:25 +0100

    fix server port issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: import.meta.env.PORT || 3000, // Use PORT environment variable or default to 3000
+  },
+});
