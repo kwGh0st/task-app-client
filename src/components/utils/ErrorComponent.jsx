295f4d40d43ac45077bede28ccf4419626b21ca4
Karol Programuje <karol341654@gmail.com>
2024-02-29 23:21:48 +0100

    fixing css

--- src/components/utils/ErrorComponent.jsx
+++ src/components/utils/ErrorComponent.jsx
@@ -6,27 +6,29 @@ const ErrorComponent = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
-      <h1 className="text-4xl font-bold mb-4">We are working really hard!</h1>
-      <div className="text-lg">
-        Apologies for the 404. If you need help please{" "}
-        <a
-          className="font-bold underline text-2xl"
-          href="https://kwgh0st.github.io/wojtachakarol/#contact"
-          target="_blank"
-        >
-          contact us.
-        </a>
-      </div>
-      <div className="mt-4">
-        <button
-          className={`
+      <div className="flex flex-col items-center justify-center text-center w-11/12">
+        <h1 className="text-4xl font-bold mb-4">We are working really hard!</h1>
+        <div className="text-lg">
+          Apologies for the 404. If you need help please{" "}
+          <a
+            className="font-bold underline text-2xl"
+            href="https://kwgh0st.github.io/wojtachakarol/#contact"
+            target="_blank"
+          >
+            contact us.
+          </a>
+        </div>
+        <div className="mt-4">
+          <button
+            className={`
             ${isPulsating ? "animate-pulse" : ""}
             bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mb-48 rounded
           `}
-          onClick={() => setIsPulsating(false)}
-        >
-          <Link to="/">Back to login page</Link>
-        </button>
+            onClick={() => setIsPulsating(false)}
+          >
+            <Link to="/">Back to login page</Link>
+          </button>
+        </div>
       </div>
     </div>
   );
